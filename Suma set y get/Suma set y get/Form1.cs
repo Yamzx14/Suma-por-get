91fd38d65de3954b0e2e2010bcdd3f5ff3cced82
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:25:51 -0500

    Add files via upload

---
+++ Suma set y get/Suma set y get/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suma_set_y_get
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sumaclass sum = new Sumaclass();
+
+            sum.N11=int.Parse(textBox1.Text);
+            sum.N12=int.Parse(textBox2.Text);
+
+            textBox3.Text=sum.Suma().ToString();
+        }
+    }
+}
---
+++ Suma set y get/Suma set y get/Suma.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_set_y_get
+{
+    internal class Sumaclass
+    {
+        
+        private int N1;
+        private int N2;
+
+       
+        public int N11 { get => N1; set => N1 = value; }
+        public int N12 { get => N2; set=>N2 = value; }
+
+        
+
+        
+        public int Suma()
+        {
+            return (N11 + N12);
+        }
+    }
+}
